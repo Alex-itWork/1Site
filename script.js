884d00e97fd01c5d7e0bee25e4b1efbcc2d3577e
Alex Gromov <162816544+Alex-itWork@users.noreply.github.com>
2025-01-01 22:06:43 +0300

    Update script.js
    199

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-// Функция для отображения или скрытия этапов работы
-function toggleStages() {
-    const container = document.querySelector('.container');
-    container.style.display = (container.style.display === 'block') ? 'none' : 'block';
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.carousel-item');
 
-// Функция для плавной прокрутки к якорю
-function smoothScroll(target) {
-    const element = document.querySelector(target);
-    if (element) {
-        element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
-}
-
-// Добавление обработчиков событий на навигационные ссылки
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            if (targetId === '#contacts') {
-                toggleStages();
-            } else {
-                smoothScroll(targetId);
+    function showSlide(index) {
+        slides.forEach((slide, idx) => {
+            slide.classList.remove('active');
+            if (idx === index) {
+                slide.classList.add('active');
             }
         });
-    });
+    }
+
+    window.moveSlide = function(direction) {
+        currentIndex += direction;
+
+        if (currentIndex < 0) {
+            currentIndex = slides.length - 1; // Зацикливание
+        } else if (currentIndex >= slides.length) {
+            currentIndex = 0; // Зацикливание
+        }
+
+        showSlide(currentIndex);
+    }
+
+    showSlide(currentIndex);
 });
